698c69c875e8dcc5ddd64a76396b53d4cbcf6f9c
novemberlc <57917926+novemberlc@users.noreply.github.com>
2024-04-24 22:12:16 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/NCMSStudios/NCMSGuides',
+            'https://github.com/NCMSStudios/NCMSGuides/tree/main/',
         },
         blog: false,
         theme: {
