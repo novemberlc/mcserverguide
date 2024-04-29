d5be31f6f4e7add05cf1d3030c3aaec2805d7113
novemberlc <57917926+novemberlc@users.noreply.github.com>
2024-04-28 20:15:37 -0400

    Update proxy guide

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['toml'],
+        additionalLanguages: ['toml', 'properties'],
       },
     }),
 };
