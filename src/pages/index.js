a86483b4960dec67d6dd798aa3398a4f6400ae06
novemberlc <57917926+novemberlc@users.noreply.github.com>
2024-09-01 01:02:43 -0400

    fixed landing page contribute url

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
                     <div style={{ textAlign: 'center', maxWidth: '500px', width: '80%' }}>
                         <h1 style={{ color: 'var(--ifm-color-primary)' }}>Welcome to the MC Server Guide</h1>
                         <p3>This is a simple, open-source website dedicated to providing people with easy-to-understand how-to guides on setting up and running Minecraft servers, ranging from small community servers to large networks.</p3>
-                        <p3>Have something to contribute? <b><Link to="https://github.com/NCMSStudios/NCMSGuides">Please do!</Link></b> We want to enable people to learn, and sharing knowledge is the best way to do that.</p3>
+                        <p3>Have something to contribute? <b><Link to="https://github.com/novemberlc/mcserverguide">Please do!</Link></b> We want to enable people to learn, and sharing knowledge is the best way to do that.</p3>
                         <div style={{ display: 'ruby', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
                             <Button onClick={handleClick}>Go to the guide!</Button>
                         </div>
